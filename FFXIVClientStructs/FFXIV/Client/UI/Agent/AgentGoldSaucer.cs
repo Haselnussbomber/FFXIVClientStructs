bd4ba61b3da69e2c4da215f5999abe437dbed693
Haselnussbomber <mail@haselnussbomber.de>
2026-05-08 00:08:04 +0200

    Add AgentGoldSaucer.EditDeckIndex

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentGoldSaucer.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentGoldSaucer.cs
@@ -10,6 +10,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 public unsafe partial struct AgentGoldSaucer {
     [FieldOffset(0x5A)] public short GoldSaucerSelectedTab;
     [FieldOffset(0x5C)] public short ChocoboSeletedTab;
+    [FieldOffset(0xFC)] public int EditDeckIndex;
     [FieldOffset(0x100)] public int EditDeckSelectedPage;
     [FieldOffset(0x108)] public int EditDeckSelectedCardIndex;
     [FieldOffset(0x10C)] public CardListFilterMode CardListFilterMode;
